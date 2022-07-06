23d5b4f30ff6a6656254a05c3342988e9966dea0
Daniel Cançado André <dandre9@gmail.com>
2022-07-06 15:44:09 -0300

    Organize project files. Add Android permission check

---
+++ Assets/Plugins/ExercisePlugin/ExerciseService.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using TMPro;
+using UnityEngine;
+using Newtonsoft.Json.Linq;
+
+public static class ExerciseService
+{
+
+    public struct ExerciseData
+    {
+        public readonly double lat;
+        public readonly double lon;
+        public readonly double alt;
+        public readonly double distance;
+        public readonly int steps;
+
+        public ExerciseData(double lat, double lon, double alt, double distance, int steps)
+        {
+            this.lat = lat;
+            this.lon = lon;
+            this.alt = alt;
+            this.distance = distance;
+            this.steps = steps;
+        }
+    }
+
+    public enum ExercisePermission
+    {
+        GRANTED = 0,
+        DENIED = 1,
+        NEVER_ASKED = 2,
+        DENIED_STEPS = 3,
+        DENIED_LOCATION = 4
+    }
+
+#if UNITY_IOS && !UNITY_EDITOR
+    [DllImport("__Internal")]
+    private static extern string _getData();
+
+    [DllImport("__Internal")]
+    private static extern void _startService();
+
+    [DllImport("__Internal")]
+    private static extern void _stopService();
+
+    [DllImport("__Internal")]
+    private static extern string _getRouteCoords();
+
+#elif UNITY_ANDROID && !UNITY_EDITOR
+    private static AndroidJavaClass unityClass = new AndroidJavaClass(UnityDefaultJavaClassName);
+    private static AndroidJavaObject unityActivity = unityClass.GetStatic<AndroidJavaObject>("currentActivity");
+    private static AndroidJavaClass customClass = new AndroidJavaClass("com.radarstudio.toast.plugin.Bridge");
+    private const string UnityDefaultJavaClassName = "com.unity3d.player.UnityPlayer";
+    private const string CustomClassStartServiceMethod = "StartService";
+    private const string CustomClassStopServiceMethod = "StopService";
+    private const string CustomClassGetDataMethod = "GetData";
+    private const string CustomClassHasPermission = "HasPermission";
+    private const string CustomClassRequestPermission = "RequestPermission";
+#endif    
+
+    public static void StartService()
+    {
+#if UNITY_IOS && !UNITY_EDITOR
+        _startService();
+#elif UNITY_ANDROID && !UNITY_EDITOR   
+        customClass.CallStatic(CustomClassStartServiceMethod);
+#endif
+    }
+
+    public static ExercisePermission HasPermission()
+    {
+        string permissionState = "";
+        ExercisePermission permission;
+
+#if UNITY_IOS && !UNITY_EDITOR
+        // TODO
+#elif UNITY_ANDROID && !UNITY_EDITOR
+        permissionState = customClass.CallStatic<string>(CustomClassHasPermission, unityActivity);
+#endif
+
+        Debug.Log("PERMISSION: " + permissionState);
+
+        switch (permissionState)
+        {
+            case "granted":
+                permission = ExercisePermission.GRANTED;
+                break;
+            case "denied":
+                permission = ExercisePermission.DENIED;
+                break;
+            case "steps":
+                permission = ExercisePermission.DENIED_STEPS;
+                break;
+            case "location":
+                permission = ExercisePermission.DENIED_LOCATION;
+                break;
+            case "neverAsked":
+                permission = ExercisePermission.NEVER_ASKED;
+                break;
+            default:
+                permission = ExercisePermission.DENIED;
+                break;
+        }
+
+        return permission;
+    }
+
+    public static void RequestPermission()
+    {
+#if UNITY_IOS && !UNITY_EDITOR
+        //TODO
+#elif UNITY_ANDROID && !UNITY_EDITOR
+        customClass.CallStatic(CustomClassRequestPermission);
+#endif
+    }
+
+    public static void StopService()
+    {
+#if UNITY_IOS && !UNITY_EDITOR
+        _stopService();
+#elif UNITY_ANDROID && !UNITY_EDITOR
+        customClass.CallStatic(CustomClassStopServiceMethod);
+#endif
+    }
+
+    public static ExerciseData GetData()
+    {
+        double[] data = { 0, 0, 0, 0, 0 };
+
+#if UNITY_IOS && !UNITY_EDITOR
+        string strData = _getData();
+        JArray json = JArray.Parse(strData);
+        data = new double[json.Count];
+        int i = 0;
+
+        foreach (double item in json)
+        {
+            data[i++] = item;
+        }
+
+#elif UNITY_ANDROID && !UNITY_EDITOR
+        data = customClass.CallStatic<double[]>(CustomClassGetDataMethod);
+#endif
+
+        return new ExerciseData(data[0], data[1], data[2], data[3], (int)data[4]);
+    }
+}
---
+++ Assets/Scripts/UserScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UserScript : MonoBehaviour
+{
+    [SerializeField] TMP_Text steps;
+    [SerializeField] TMP_Text distance;
+    [SerializeField] TMP_Text coords;
+    [SerializeField] TMP_Text altitude;
+    [SerializeField] TMP_Text serviceRunning;
+    // [SerializeField] LineRenderer route;
+    // [SerializeField] CanvasGroup canvasGroup;
+
+    ExerciseService.ExerciseData exerciseData;
+
+    public void StartTraining()
+    {
+        ExerciseService.ExercisePermission permission = ExerciseService.HasPermission();
+
+        if (permission == ExerciseService.ExercisePermission.GRANTED)
+        {
+            serviceRunning.text = "Ligado";
+            serviceRunning.color = Color.green;
+
+            ExerciseService.StartService();
+            InvokeRepeating("UpdateExerciseData", 1, 5);
+        }
+        else if (permission == ExerciseService.ExercisePermission.NEVER_ASKED)
+        {
+            ExerciseService.RequestPermission();
+        }
+        else
+        {
+            if (permission == ExerciseService.ExercisePermission.DENIED)
+                Debug.Log("É NECESSÁRIO TER AS DUAS PERMISSOES");
+            else if (permission == ExerciseService.ExercisePermission.DENIED_LOCATION)
+                Debug.Log("É NECESSÁRIO TER LOCALIZAÇÃO");
+            else if (permission == ExerciseService.ExercisePermission.DENIED_STEPS)
+                Debug.Log("É NECESSÁRIO TER ACELEROMETRO");
+        }
+    }
+
+    void UpdateExerciseData()
+    {
+        exerciseData = ExerciseService.GetData();
+
+        steps.text = exerciseData.steps.ToString();
+        distance.text = exerciseData.distance.ToString("N2") + " m";
+        coords.text = exerciseData.lat.ToString("N7") + " , " + exerciseData.lon.ToString("N7");
+        altitude.text = exerciseData.alt.ToString("N2") + " (altitude)";
+    }
+
+    public void StopTraning()
+    {
+        serviceRunning.text = "Desligado";
+        serviceRunning.color = Color.red;
+
+        ExerciseService.StopService();
+    }
+
+}
